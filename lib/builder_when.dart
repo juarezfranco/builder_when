b4928f5cfa8b491b02d4cf67de343477b5144790
Juarez Franco <juarezfrancojr@gmail.com>
2023-11-20 16:02:06 -0300

    dart format .

--- lib/builder_when.dart
+++ lib/builder_when.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/widgets.dart';
+
 /// Conditional build creator
 class BuilderWhen extends StatelessWidget {
   /// Flag to build
   final bool condition;
+
   /// Closure-based builder for when condition is true
   final Widget Function(BuildContext) isTrue;
+
   /// Closure-based builder for when condition is false
   final Widget Function(BuildContext) isFalse;
 
