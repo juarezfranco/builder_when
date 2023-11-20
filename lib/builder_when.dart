00c3cef5a2317b53c54fd36c9e6e3540483e7121
Bruno Moraes <it.brunoms@gmail.com>
2023-11-20 16:15:21 -0300

    feat: created BuilderWhenClosure for builder closuse

--- lib/builder_when.dart
+++ lib/builder_when.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/widgets.dart';
 
+///Type definition for builder closure
+typedef BuilderWhenClosure = Widget Function(BuildContext);
+
 /// Conditional build creator
 class BuilderWhen extends StatelessWidget {
   /// Flag to build
   final bool condition;
 
   /// Closure-based builder for when condition is true
-  final Widget Function(BuildContext) isTrue;
+  final BuilderWhenClosure isTrue;
 
   /// Closure-based builder for when condition is false
-  final Widget Function(BuildContext) isFalse;
+  final BuilderWhenClosure isFalse;
 
   const BuilderWhen({
     Key? key,
